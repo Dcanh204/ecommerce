4738d9c0e950505c78d93c11e6cbd28b10a40485
Dinh Canh <dinhcanhh2004@gmail.com>
2025-11-16 23:45:34 +0700

    feat: add category component UI

---
+++ src/components/Category.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+import { Link } from 'react-router-dom';
+const Category = () => {
+
+  const categories = [
+    'Điện thoại',
+    'Tivi',
+    'Đồng Hồ',
+    'Tủ lạnh',
+    'Máy tính',
+    'Máy giặt',
+    'Điều hòa'
+  ]
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 6
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4
+    },
+    mdtablet: {
+      breakpoint: { max: 991, min: 464 },
+      items: 4
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3
+    },
+    smmobile: {
+      breakpoint: { max: 440, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <div className='w-[90%] mt-6 mx-auto'>
+      <div className='w-full flex justify-center items-center flex-col text-slate-600 pb-5 md:pb-[35px]'>
+        <h2 className='text-center text-xl md:text-3xl font-bold'>Danh mục hàng đầu</h2>
+        <div className='w-[100px] h-0.5 bg-[#059473] mt-4'></div>
+      </div>
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        infinite={true}
+        transitionDuration={500}
+      >
+        {
+          categories.map((c, i) => <Link className='h-[185px] md:h-[300px] block border border-slate-100' key={i} to='#'>
+            <div className='w-full h-full relative p-3'>
+              <img src={`/images/products/${i + 1}.webp`} alt="" />
+              <div className='absolute bottom-4 w-full mx-auto font-bold left-0 flex justify-center items-center'>
+                <span className='py-0.5 px-6 bg-[#b9b7b7]/80 text-white rounded-md'>{c}</span>
+              </div>
+            </div>
+          </Link>)
+        }
+      </Carousel>
+    </div>
+  );
+};
+
+export default Category;
